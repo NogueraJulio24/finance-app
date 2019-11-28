d31796ed045fde6c3fcc32c0479420da7f90658e
Julio Noguera <noguerajulio24@gmail.com>
2019-11-27 20:36:21 -0500

    adding alert page

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -6,7 +6,7 @@ class AlertPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Alert"),
+        title: Text("Alert Page"),
       ),
     );
   }
