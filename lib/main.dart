868b3471693187777e0511b8d76ea1d884bb27e8
Julio Noguera <noguerajulio24@gmail.com>
2019-11-27 21:05:05 -0500

    adding route in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:finance_app/src/pages/home_page.dart';
+import 'package:finance_app/src/pages/avatar_page.dart';
+import 'package:finance_app/src/pages/alert_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,7 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Components app',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/' : ( BuildContext context ) => HomePage(),
+        'alert' : ( BuildContext context ) => AlertPage(),
+        'avatar' : (BuildContext context) => AvatarPage()
+      },
     );
   }
 }
